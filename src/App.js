ed8cbd63d2ca10bd976fe5e518c63c73749dfee6
Michael <trinh.michael98@gmail.com>
2022-08-05 02:27:11 -0700

    375px mobile view gtg

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import AdviceCard from './components/advice-card';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import Dice from "./images/icon-dice.svg";
+import DividerMobile from "./images/pattern-divider-mobile.svg";
 
-function App() {
-  
+function AdviceCard() {
+  const [ID, setID] = useState("");
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setID(data.slip.id);
+        setAdvice(data.slip.advice);
+      });
+  }, [ID, advice]);
 
   return (
-    <>
-      <AdviceCard />
-    </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="wrapper">
+      <div className="flexContainer">
+        <div className="top">
+          <h1>A D V I C E &nbsp; #&nbsp;{ID}</h1>
+        </div>
+        <div className="middle">
+          <h2>"{advice}"</h2>
+        </div>
+        <div className="bottom">
+          <img src={DividerMobile} alt="Divider" />
+        </div>
+        <div
+          className="diceWrapper"
+          onClick={() => {
+            setID("");
+            setAdvice("");
+          }}
+        >
+          <img src={Dice} alt="Generate New Random Advice" />
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default AdviceCard;
